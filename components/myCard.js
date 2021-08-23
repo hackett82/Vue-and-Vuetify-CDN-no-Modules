4aef8d20cd8e4329e4f70e728c38ea5eb3901458
hackett82 <69408410+hackett82@users.noreply.github.com>
2021-08-23 06:00:45 -0700

    added a prop for the card to pass data

--- components/myCard.js
+++ components/myCard.js
@@ -1,31 +1,38 @@
 const MyCard = {
-template: `
-  <div>
-    <v-card
-        class="mx-auto"
-        max-width="344"
-        outlined
-    >
-        <v-list-item three-line>
-        <v-list-item-content>
-            <div class="overline mb-4">OVERLINE</div>
-            <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
-            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-        </v-list-item-content>
+  props: {
+    card: {
+        type: Object,
+        required: true
+      },
+  },
+  template: `
+    <div>
+      <v-card
+          class="mx-auto"
+          max-width="344"
+          outlined
+          color="#CCC9DC"
+      >
+          <v-list-item three-line>
+          <v-list-item-content>
+              <div class="overline mb-4">OVERLINE</div>
+              <v-list-item-title class="headline mb-1">{{ card.description }}</v-list-item-title>
+              <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
 
-        <v-list-item-avatar
-            tile
-            size="80"
-            color="grey"
-        ></v-list-item-avatar>
-        </v-list-item>
+          <v-list-item-avatar
+              tile
+              size="80"
+              :color="card.color"
+          ></v-list-item-avatar>
+          </v-list-item>
 
-        <v-card-actions>
-        <v-btn text>Button</v-btn>
-        <v-btn text>Button</v-btn>
-        </v-card-actions>
-    </v-card>
-    
-  </div>
-  `
+          <v-card-actions>
+          <v-btn text>Button</v-btn>
+          <v-btn text>Button</v-btn>
+          </v-card-actions>
+      </v-card>
+      
+    </div>
+  `,
 }
